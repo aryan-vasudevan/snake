31ad20c451639bec9e24ed73f41ff3ea0baa6dae
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2023-03-10 22:20:36 -0500

    Made a moving snake that moves every 150 milliseconds.

--- main.py
+++ main.py
@@ -4,21 +4,29 @@ from pygame.math import Vector2
 # Initialize Pygame
 pygame.init()
 
-# Create screen and cell size
+# Create screen and essentials
 cell_size = 20
 cell_number = 30
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 pygame.display.set_caption("Snake")
 
+clock = pygame.time.Clock()
+
 # Snake class
 class Snake:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        self.direction = Vector2(1, 0)
 
     def draw(self):
         for block in self.body:
             block_rect = pygame.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size)
             pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+    def move(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy[:]
 
 # Fruit class
 class Fruit:
@@ -36,12 +44,18 @@ snake = Snake()
 fruit = Fruit()
 
 # Game loop
+screen_update = pygame.USEREVENT
+pygame.time.set_timer(screen_update, 150)
+
 running = True
 while running:
     # Check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+        if event.type == screen_update:
+            snake.move()
 
     fruit.draw()
     snake.draw()
